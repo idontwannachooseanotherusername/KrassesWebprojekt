12eb52a229ae3204443e23827f0d13e751d54c22
idontwannachooseanotherusername <harmsnic@hs-albsig.de>
2022-03-18 22:34:03 +0100

    Added user icon with dropdown menu

--- Frontend/scripts/main.js
+++ Frontend/scripts/main.js
@@ -1,19 +1,19 @@
 function show_dropdown(){
     var drop = document.getElementById("menu-dropdown");
-    if (drop.style.display == "none"){
+    if (drop.style.display != "revert"){
         drop.style.display = "revert";
+        console.log('Dropdown visible');
     }
     else{
         drop.style.display = "none";
+        console.log('Dropdown hidden');
     }
 }
 
-//https://www.geeksforgeeks.org/password-matching-using-javascript/
 function check_password(){
     let new_pw = document.getElementById("new-pw");
     let rep_pw = document.getElementById("rep-pw");
     let errors = document.getElementById("errors");
-    let button = document.getElementById("pw-submit");
     
     if (new_pw.value != rep_pw.value){
         errors.innerHTML = "Passwords do not match"
@@ -24,3 +24,12 @@ function check_password(){
         return true
     }
 }
+
+window.onclick = e => {
+    var drop = document.getElementById("menu-dropdown");
+    if (!(drop.contains(e.target)) && e.target.getAttribute('id') != "pb"){
+        drop.style.display = "none";
+        console.log('Dropdown hidden');
+    }
+}
+
