a00fd145d822faa5a69ed74a8a951bd06fb0d86e
idontwannachooseanotherusername <harmsnic@hs-albsig.de>
2022-03-30 13:15:38 +0200

    Changed some DB names

--- Backend/db/InsertTestdata.sql
+++ Backend/db/InsertTestdata.sql
@@ -1,8 +1,9 @@
 INSERT INTO Category (Title) VALUES ('Math');
-INSERT INTO Category (Title) VALUES ('Logic');
+INSERT INTO Category (Title) VALUES ('Logic'); 
 
-INSERT INTO User (Username, Password, Bio, PicturePath, CountryID, Points) VALUES ('User1', '123456', 'Nothing to see here', '', 1, 1000);
-INSERT INTO User (Username, Password, Bio, PicturePath, CountryID, Points) VALUES ('User2', 'abcde', 'Great Bio', '../../Frontend/images/profile-1.png', 2, 2000);
+INSERT INTO User (Username, Password, Bio, PicturePath, CountryID, Points, Deleted) VALUES ('User1', '123456', 'Nothing to see here', '', 1, 1000, 0);
+INSERT INTO User (Username, Password, Bio, PicturePath, CountryID, Points, Deleted) VALUES ('User2', 'abcde', 'Great Bio', '../../Frontend/images/profile-1.png', 2, 2000, 0);
+INSERT INTO User (Username, Password, Bio, PicturePath, CountryID, Points, Deleted) VALUES ('User3-del', '', '', '', null, 0, 1);
 
 INSERT INTO Tag (PicturePath, Title) VALUES ('../../Frontend/images/icons/internet.png', 'Internet required');
 INSERT INTO Tag (PicturePath, Title) VALUES ('../../Frontend/images/icons/knowledge.png', 'Special knowledge');
@@ -33,6 +34,6 @@ INSERT INTO Challengetag (ChallengeID, TagID) VALUES (1, 2);
 INSERT INTO Challengetag (ChallengeID, TagID) VALUES (1, 3);
 INSERT INTO Challengetag (ChallengeID, TagID) VALUES (2, 4);
 
-INSERT INTO Challengepicture (ChallengeID, PicturePath) VALUES (1, '../Data/image.png');
-INSERT INTO Challengepicture (ChallengeID, PicturePath) VALUES (2, '../Data/image.png');
+INSERT INTO Challengefile (ChallengeID, FilePath) VALUES (1, '../Data/image.png');
+INSERT INTO Challengefile (ChallengeID, FilePath) VALUES (2, '../Data/image.png');
 
