d9d37c5f7dafc93e18315f897c7880c6694f1ceb
idontwannachooseanotherusername <harmsnic@hs-albsig.de>
2022-04-03 23:10:09 +0200

    Fixed getting solved from db

--- Backend/services/solved.js
+++ Backend/services/solved.js
@@ -3,52 +3,52 @@ const SolvedDao = require('../dao/solvedDao.js');
 const express = require('express');
 var serviceRouter = express.Router();
 
-helper.log('- Service Challengefile');
+helper.log('- Service Solved');
 
 serviceRouter.get('/solved/get/:id', function(request, response) {
-    helper.log('Service Challengefile: Client requested one record, id=' + request.params.id);
+    helper.log('Service Solved: Client requested one record, id=' + request.params.id);
 
     const solvedDao = new SolvedDao(request.app.locals.dbConnection);
     try {
         var result = solvedDao.loadById(request.params.id);
-        helper.log('Service Challengefile: Record loaded');
+        helper.log('Service Solved: Record loaded');
         response.status(200).json(helper.jsonMsgOK(result));
     } catch (ex) {
-        helper.logError('Service Challengefile: Error loading record by id. Exception occured: ' + ex.message);
+        helper.logError('Service Solved: Error loading record by id. Exception occured: ' + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
 });
 
 serviceRouter.get('/solved/all/', function(request, response) {
-    helper.log('Service Challengefile: Client requested all records');
+    helper.log('Service Solved: Client requested all records');
 
     const solvedDao = new SolvedDao(request.app.locals.dbConnection);
     try {
         var result = solvedDao.loadAll();
-        helper.log('Service Challengefile: Records loaded, count=' + result.length);
+        helper.log('Service Solved: Records loaded, count=' + result.length);
         response.status(200).json(helper.jsonMsgOK(result));
     } catch (ex) {
-        helper.logError('Service Challengefile: Error loading all records. Exception occured: ' + ex.message);
+        helper.logError('Service Solved: Error loading all records. Exception occured: ' + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
 });
 
 serviceRouter.get('/solved/exists/:id', function(request, response) {
-    helper.log('Service Challengefile: Client requested check, if record exists, id=' + request.params.id);
+    helper.log('Service Solved: Client requested check, if record exists, id=' + request.params.id);
 
     const solvedDao = new SolvedDao(request.app.locals.dbConnection);
     try {
         var result = solvedDao.exists(request.params.id);
-        helper.log('Service Challengefile: Check if record exists by id=' + request.params.id + ', result=' + result);
+        helper.log('Service Solved: Check if record exists by id=' + request.params.id + ', result=' + result);
         response.status(200).json(helper.jsonMsgOK({ 'id': request.params.id, 'existiert': result }));
     } catch (ex) {
-        helper.logError('Service Challengefile: Error checking if record exists. Exception occured: ' + ex.message);
+        helper.logError('Service Solved: Error checking if record exists. Exception occured: ' + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
 });
 
 serviceRouter.post('/solved', function(request, response) {
-    helper.log('Service Challengefile: Client requested creation of new record');
+    helper.log('Service Solved: Client requested creation of new record');
 
     var errorMsgs=[];
     if (helper.isUndefined(request.body.filepath)) 
@@ -56,7 +56,7 @@ serviceRouter.post('/solved', function(request, response) {
     if (helper.isUndefined(request.body.id)) 
         errorMsgs.push('id missing');
     if (errorMsgs.length > 0) {
-        helper.log('Service Challengefile: Creation not possible, data missing');
+        helper.log('Service Solved: Creation not possible, data missing');
         response.status(400).json(helper.jsonMsgError('Creation not possible, missing data: ' + helper.concatArray(errorMsgs)));
         return;
     }
@@ -64,16 +64,16 @@ serviceRouter.post('/solved', function(request, response) {
     const solvedDao = new SolvedDao(request.app.locals.dbConnection);
     try {
         var result = solvedDao.create(request.body.filepath, request.body.id);
-        helper.log('Service Challengefile: Record inserted');
+        helper.log('Service Solved: Record inserted');
         response.status(200).json(helper.jsonMsgOK(result));
     } catch (ex) {
-        helper.logError('Service Challengefile: Error creating new record. Exception occured: ' + ex.message);
+        helper.logError('Service Solved: Error creating new record. Exception occured: ' + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
 });
 
 serviceRouter.put('/solved', function(request, response) {
-    helper.log('Service Challengefile: Client requested update of existing record');
+    helper.log('Service Solved: Client requested update of existing record');
 
     var errorMsgs=[];
     if (helper.isUndefined(request.body.filepath)) 
@@ -81,7 +81,7 @@ serviceRouter.put('/solved', function(request, response) {
     if (helper.isUndefined(request.body.id)) 
         errorMsgs.push('id missing');
     if (errorMsgs.length > 0) {
-        helper.log('Service Challengefile: Creation not possible, data missing');
+        helper.log('Service Solved: Creation not possible, data missing');
         response.status(400).json(helper.jsonMsgError('Creation not possible, missing data: ' + helper.concatArray(errorMsgs)));
         return;
     }
@@ -89,25 +89,25 @@ serviceRouter.put('/solved', function(request, response) {
     const solvedDao = new SolvedDao(request.app.locals.dbConnection);
     try {
         var result = solvedDao.create(request.body.filepath, request.body.id);
-        helper.log('Service Challengefile: Record updated, id=' + request.body.id);
+        helper.log('Service Solved: Record updated, id=' + request.body.id);
         response.status(200).json(helper.jsonMsgOK(result));
     } catch (ex) {
-        helper.logError('Service Challengefile: Error updating record by id. Exception occured: ' + ex.message);
+        helper.logError('Service Solved: Error updating record by id. Exception occured: ' + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }    
 });
 
 serviceRouter.delete('/solved/:id', function(request, response) {
-    helper.log('Service Challengefile: Client requested deletion of record, id=' + request.params.id);
+    helper.log('Service Solved: Client requested deletion of record, id=' + request.params.id);
 
     const solvedDao = new SolvedDao(request.app.locals.dbConnection);
     try {
         var obj = solvedDao.loadById(request.params.id);
         solvedDao.delete(request.params.id);
-        helper.log('Service Challengefile: Deletion of record successfull, id=' + request.params.id);
+        helper.log('Service Solved: Deletion of record successfull, id=' + request.params.id);
         response.status(200).json(helper.jsonMsgOK({ 'deleted': true, 'entry': obj }));
     } catch (ex) {
-        helper.logError('Service Challengefile: Error deleting record. Exception occured: ' + ex.message);
+        helper.logError('Service Solved: Error deleting record. Exception occured: ' + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
 });
